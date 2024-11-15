6a0fb81978d7b25cbadf7149ef2032ba1b501f7c
Dlowyz <artur.lomp@voco.ee>
2024-11-15 15:38:30 +0200

    React4

--- expenses/src/components/ExpenseItem.js
+++ expenses/src/components/ExpenseItem.js
@@ -1,10 +1,9 @@
 import './ExpenseItem.css';
-
+import ExpenseDate from './ExpenseDate';
 const ExpenseItem = (props) => {
-    console.log(props);
     return(
     <div className='expense-item'>
-        <div>{props.data.date.toString()}</div>
+        <ExpenseDate date={props.data.date}/>
         <div className='expense-item-description'>
             <h2>{props.data.title}</h2>
             <div className='expense-item-price'>{props.data.price}</div>
