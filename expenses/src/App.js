68389e0fe6697107b22a4c502f3746acdc4672e1
Dlowyz <artur.lomp@voco.ee>
2024-11-15 15:30:07 +0200

    React3

--- expenses/src/App.js
+++ expenses/src/App.js
@@ -3,10 +3,24 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+const expenses = [
+  {
+  date: new Date(2021, 2, 28),
+  title: 'Car Insurance',
+  price: 294.67
+  },
+  {
+  date: new Date(2023, 5, 12),
+  title: 'Home Insurance',
+  price: 549.99
+  }
+]
+
   return (
     <div className="App">
-      <ExpenseItem/>
-      <ExpenseItem/>
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
+      
     </div>
   );
 }
