4bf24433d8fa7c78a0c35b8c82fd3d3053636c2e
Dlowyz <artur.lomp@voco.ee>
2024-11-21 18:22:19 +0200

    7

--- expenses/src/components/ExpenseItem.js
+++ expenses/src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 const ExpenseItem = (props) => {
+    console.log(props)
     return(
     <div className='expense-item'>
+        
+        <div className='DateContainer'>
         <ExpenseDate date={props.data.date}/>
+        </div>
         <div className='expense-item-description'>
             <h2>{props.data.title}</h2>
             <div className='expense-item-price'>{props.data.price}</div>
